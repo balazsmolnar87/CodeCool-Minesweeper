61b37d1f9fe379ea04f1068a0f7798de8df18b63
balazsmolnar87 <balazs.millair@gmail.com>
2022-10-08 15:16:54 +0200

    Implement getRandomMineIndexes method

--- js/game.js
+++ js/game.js
@@ -22,6 +22,15 @@
             }
         }
     },
+
+    getRandomMineIndexes: function (mineCount, cols, rows) {
+        const cellCount = cols * rows;
+        let mines = new Set();
+        do {
+            mines.add(Math.round(Math.random() * (cellCount - 1)));
+        } while (mines.size < mineCount && mines.size < cellCount);
+        return mines;
+    },
 };
 
 game.init();
