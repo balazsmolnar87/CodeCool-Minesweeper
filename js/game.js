9dbfbb7528fd1c01f2a3f13b4bc728028f7b6365
balazsmolnar87 <balazs.millair@gmail.com>
2022-10-11 18:53:20 +0200

    Refactor using no modules

--- js/game.js
+++ js/game.js
@@ -1,43 +1,16 @@
-﻿const flagsLeftCounter = document.querySelector('#flags-left-counter');
-let isGameOver = false;
-let flagsLeft = 0;
-let mines = 0;
+﻿const game = {
+    flagsLeftCounter: document.querySelector('#flags-left-counter'),
+    isGameOver : false,
+    flagsLeft : 0,
+    mines : 0,
 
-function checkForWin() {
-    const fields = document.querySelectorAll('.game-field .row .field');
-    
-    let total = 0;
-    for (let field of fields) {
-        if (field.classList.contains("mine") && field.classList.contains("flagged")) total++;
-    }
-    if (total === mines) {
-        alert('You won!')
-        isGameOver = true;
-    }    
-}
-
-function gameOver() {
-    const fields = document.querySelectorAll('.game-field .row .field');
-    
-    for (let field of fields) {
-        if (field.classList.contains("mine")) {
-            field.classList.add("open")   
-        }
-    }
-    
-    alert('Boom! Game over!');
-    isGameOver = true;
-}
-
-const game = {
     init: function () {
         //Create the board
-        this.drawBoard();
+        game.drawBoard();
 
         //What should happen when we click on a tile    
         this.initRightClick();
         this.initLeftClick();
-
     },
 
     drawBoard: function () {
@@ -46,12 +19,12 @@ const game = {
         const rows = parseInt(urlParams.get('rows'));
         const cols = parseInt(urlParams.get('cols'));
         const mineCount = parseInt(urlParams.get('mines'));
-        mines = mineCount;
+        game.mines = mineCount;
         const minePlaces = this.getRandomMineIndexes(mineCount, cols, rows);
         
         //Set how many flags do we have
-        flagsLeftCounter.setAttribute("value", mineCount.toString());
-        flagsLeft = mineCount;
+        game.flagsLeftCounter.setAttribute("value", mineCount.toString());
+        game.flagsLeft = mineCount;
 
         let gameField = document.querySelector('.game-field');
         this.setGameFieldSize(gameField, rows, cols);
@@ -98,92 +71,123 @@ const game = {
 
     initRightClick: function () {
         const fields = document.querySelectorAll('.game-field .row .field');
-        
+
+        function addOrRemoveFlag(field) {
+            if (!field.classList.contains('flagged') && game.flagsLeft > 0){
+                field.classList.add('flagged');
+                game.flagsLeft--;
+                game.flagsLeftCounter.setAttribute("value", game.flagsLeft.toString());
+                checkForWin();
+            } else {
+                if (field.classList.contains('flagged')){
+                    field.classList.remove('flagged');
+                    game.flagsLeft++;
+                    game.flagsLeftCounter.setAttribute("value", game.flagsLeft.toString());
+                }
+            }
+        }
+
         for (let field of fields) {
             field.addEventListener('contextmenu', function (event) {
                 event.preventDefault();
-                if (isGameOver) return;
+                if (game.isGameOver) return;
                 if (!field.classList.contains('opened')){
-                    if (!field.classList.contains('flagged') && flagsLeft > 0){
-                        field.classList.add('flagged');
-                        flagsLeft--;
-                        flagsLeftCounter.setAttribute("value", flagsLeft.toString());
-                        checkForWin();
-                    } else {
-                        if (field.classList.contains('flagged')){
-                            field.classList.remove('flagged');
-                            flagsLeft++;
-                            flagsLeftCounter.setAttribute("value", flagsLeft.toString());
-                        }
-                    }
+                    addOrRemoveFlag(field);
                 }
             });
         }
     },
-    
+
     initLeftClick: function () {
         const fields = document.querySelectorAll('.game-field .row .field');
-        
-        for (let field of fields) {
-            field.addEventListener('click', function (event) {
-                if (isGameOver) return;
-                if (field.classList.contains('open') || field.classList.contains('flagged')) return;
 
-                function countAdjacentMines(field) {
-                    let total = 0;
+        function leftClick(field) {
+            if (game.isGameOver) return;
+            if (field.classList.contains('open') || field.classList.contains('flagged')) return;
+
+            function countAdjacentMines(field) {
+                let total = 0;
+                let row = Number(field.getAttribute('data-row'));
+                let col = Number(field.getAttribute('data-col'));
+
+                for (let f of fields){
+                    let fRow = Number(f.getAttribute('data-row'));
+                    let fCol = Number(f.getAttribute('data-col'));
+
+                    if      (fRow === row-1 && fCol === col-1 && f.classList.contains('mine')) total++;
+                    else if (fRow === row-1 && fCol === col   && f.classList.contains('mine')) total++;
+                    else if (fRow === row-1 && fCol === col+1 && f.classList.contains('mine')) total++;
+                    else if (fRow === row   && fCol === col-1 && f.classList.contains('mine')) total++;
+                    else if (fRow === row   && fCol === col+1 && f.classList.contains('mine')) total++;
+                    else if (fRow === row+1 && fCol === col-1 && f.classList.contains('mine')) total++;
+                    else if (fRow === row+1 && fCol === col   && f.classList.contains('mine')) total++;
+                    else if (fRow === row+1 && fCol === col+1 && f.classList.contains('mine')) total++;
+                }
+                return total;
+            }
+
+            function recur() {
+                for (let cell of fields){
                     let row = Number(field.getAttribute('data-row'));
                     let col = Number(field.getAttribute('data-col'));
 
-                    for (let f of fields){
-                        let fRow = Number(f.getAttribute('data-row'));
-                        let fCol = Number(f.getAttribute('data-col'));
-                        if (fRow == row -1 && fCol == col -1 && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row -1 && fCol == col && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row -1 && fCol == col +1 && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row && fCol == col -1 && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row && fCol == col +1 && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row +1 && fCol == col -1 && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row +1 && fCol == col && f.classList.contains('mine')){
-                            total++;
-                        }
-                        if (fRow == row +1 && fCol == col +1 && f.classList.contains('mine')){
-                            total++;
-                        }
-                    }
-                    return total;
-                }
+                    let cRow = Number(cell.getAttribute('data-row'));
+                    let cCol = Number(cell.getAttribute('data-col'));
 
-                function recur() {
-                    //todo
+                    if (cRow === row-1 && cCol === col-1) leftClick(cell);
+                    if (cRow === row-1 && cCol === col)   leftClick(cell);
+                    if (cRow === row-1 && cCol === col+1) leftClick(cell);
+                    if (cRow === row   && cCol === col-1) leftClick(cell);
+                    if (cRow === row   && cCol === col+1) leftClick(cell);
+                    if (cRow === row+1 && cCol === col-1) leftClick(cell);
+                    if (cRow === row+1 && cCol === col)   leftClick(cell);
+                    if (cRow === row+1 && cCol === col+1) leftClick(cell);
                 }
+            }
 
-                if (field.classList.contains('mine')) {
-                    gameOver();
-                } else {
-                    let total = countAdjacentMines(field);
-                    if (total != 0) {
-                        field.classList.add('open');
-                        field.innerHTML = total;
-                        return;
-                    }
-                    recur();
+            if (field.classList.contains('mine')) {
+                gameOver();
+            } else {
+                let total = countAdjacentMines(field);
+                if (total !== 0) {
+                    field.classList.add('open');
+                    field.innerHTML = total;
+                    return;
                 }
-                field.classList.add('open');
-            });
+                recur();
+            }
+            field.classList.add('open');
+        }
+
+        for (let field of fields) {
+            field.addEventListener('click', () => leftClick(field));
         }
     },
 };
+
+function checkForWin() {
+    const fields = document.querySelectorAll('.game-field .row .field');
+    
+    let total = 0;
+    for (let field of fields) {
+        if (field.classList.contains("mine") && field.classList.contains("flagged")) total++;
+    }
+    if (total === game.mines) {
+        alert('You won!')
+        game.isGameOver = true;
+    }    
+}
+
+function gameOver() {
+    const fields = document.querySelectorAll('.game-field .row .field');
+    for (let field of fields) {
+        if (field.classList.contains("mine")) {
+            field.classList.add("open")
+        }
+    }
+
+    alert('Boom! Game over!');
+    game.isGameOver = true;
+}
 
 game.init();
